df0a42e78ca4b12044a2b74d2eb5d633145101f0
Matthias Cullmann <matthias.cullmann@baloise.ch>
2017-11-01 16:18:45 +0100

    parse github / gitea / gogs json POST requests

--- src/main/java/org/jenkinsci/plugins/createandrunjobtrigger/CreateAndRunJobTriggerPlugin.java
+++ src/main/java/org/jenkinsci/plugins/createandrunjobtrigger/CreateAndRunJobTriggerPlugin.java
@@ -7,17 +7,39 @@ import org.kohsuke.stapler.StaplerRequest;
 
 import hudson.Plugin;
 import hudson.util.HttpResponses;
+import java.util.Enumeration;
+import net.sf.json.JSONObject;
+import net.sf.json.groovy.JsonSlurper;
 
 public class CreateAndRunJobTriggerPlugin extends Plugin {
 
 	public HttpResponse doTrigger(StaplerRequest request) throws IOException {
-		String url = request.getParameter("url");
+            Enumeration<String> names = request.getHeaderNames();
+           
+            
+            while(names.hasMoreElements()) {
+                String name = names.nextElement();
+                System.out.println(name +  " = "+ request.getHeader(name));
+            }
+            if("POST".equalsIgnoreCase(request.getMethod()) 
+                    && request.getHeader("Content-Type").toLowerCase().contains("json")  ) {
+               JsonSlurper slurp = new JsonSlurper();
+               JSONObject json = (JSONObject) slurp.parse(request.getInputStream());
+                System.out.println(json);
+               if("refs/heads/master".equalsIgnoreCase(json.getString("ref"))) {
+                  String url = json.getJSONObject("repository").getString("clone_url");
+                  new OnTrigger(url).run();
+               }
+                
+            } else {
+                String url = request.getParameter("url");
 
-		if (url == null) {
-			return HttpResponses.error(400, "bad request: url parameter must be set");
-		}
-		new OnTrigger(url).run();
-
-		return HttpResponses.ok();
+                if (url == null) {
+                        return HttpResponses.error(400, "bad request: url parameter must be set");
+                }
+                new OnTrigger(url).run();
+            }
+  
+            return HttpResponses.ok();
 	}
 }
