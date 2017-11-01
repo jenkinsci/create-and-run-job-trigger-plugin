98840970fe6494f37fb7b5dec7cae4eab1fa9722
Matthias Cullmann <matthias.cullmann@baloise.ch>
2017-11-01 16:35:43 +0100

    removed debug output

--- src/main/java/org/jenkinsci/plugins/createandrunjobtrigger/CreateAndRunJobTriggerPlugin.java
+++ src/main/java/org/jenkinsci/plugins/createandrunjobtrigger/CreateAndRunJobTriggerPlugin.java
@@ -14,18 +14,10 @@ import net.sf.json.groovy.JsonSlurper;
 public class CreateAndRunJobTriggerPlugin extends Plugin {
 
 	public HttpResponse doTrigger(StaplerRequest request) throws IOException {
-            Enumeration<String> names = request.getHeaderNames();
-           
-            
-            while(names.hasMoreElements()) {
-                String name = names.nextElement();
-                System.out.println(name +  " = "+ request.getHeader(name));
-            }
             if("POST".equalsIgnoreCase(request.getMethod()) 
                     && request.getHeader("Content-Type").toLowerCase().contains("json")  ) {
                JsonSlurper slurp = new JsonSlurper();
                JSONObject json = (JSONObject) slurp.parse(request.getInputStream());
-                System.out.println(json);
                if("refs/heads/master".equalsIgnoreCase(json.getString("ref"))) {
                   String url = json.getJSONObject("repository").getString("clone_url");
                   new OnTrigger(url).run();
